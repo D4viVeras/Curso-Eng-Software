72a94b361306acfc24893600b2e225a701b9c7da
Davi Veras <146132666+D4viVeras@users.noreply.github.com>
2025-11-23 22:19:36 -0300

    Desenvolvimento de Interfaces
    Upload do Pré-Projeto de Desenvolvimento de Interfaces

---
+++ DI-2-semestre/meu-quiz-interativo/questions.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Qual tag é usada para fazer a conexão com o arquivo CSS externo?",
+        answer: "B. &lt;link&gt;",
+        options: [
+            "A. &lt;style&gt;",
+            "B. &lt;link&gt;",
+            "C. &lt;script&gt;",
+            "D. &lt;header&gt;"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qual atributo do HTML é essencial para o JavaScript selecionar um elemento específico e único?",
+        answer: "D. id",
+        options: [
+            "A. class",
+            "B. name",
+            "C. data-id",
+            "D. id"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qual tag define o conteúdo principal e visível (texto, imagens, links) de uma página HTML?",
+        answer: "B. &lt;body&gt;",
+        options: [
+            "A. &lt;head&gt;",
+            "B. &lt;body&gt;",
+            "C. &lt;section&gt;",
+            "D. &lt;main&gt;",
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "Qual propriedade CSS cria espaço exterior entre um elemento e seus vizinhos?",
+        answer: "C. margin",
+        options: [
+            "A. padding",
+            "B. border",
+            "C. margin",
+            "D. spacing",
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "Qual valor da propriedade display é usado para colocar elementos de lista um ao lado do outro na mesma linha?",
+        answer: "B. display:inline-block",
+        options: [
+            "A. display:block",
+            "B. display:inline-block",
+            "C. display:grid",
+            "D. display:flex",
+        ]
+    },
+    {
+        numb: 6,
+        question: "Qual propriedade CSS, combinada com transition, cria o efeito de zoom suave no hover?",
+        answer: "C. transform",
+        options: [
+            "A. position",
+            "B. opacity",
+            "C. transform",
+            "D. filter",
+        ]
+    },
+    {
+        numb: 7,
+        question: "Qual método do JS é usado para criar novos elementos HTML (tags) que ainda não existem na página?",
+        answer: "B. createElement()",
+        options: [
+            "A. getElementById()",
+            "B. createElement()",
+            "C. appendChild()",
+            "D. querySelector()",
+        ]
+    },
+    {
+        numb: 8,
+        question: "Qual a principal finalidade da sintaxe () => (Arrow Function)?",
+        answer: "B. Sintaxe curta para funções",
+        options: [
+            "A. Criar um Array",
+            "B. Sintaxe curta para funções",
+            "C. Criar objetos",
+            "D. Fazer loops",
+        ]
+    },
+    {
+        numb: 9,
+        question: "O que o método addEventListener() permite ao JS fazer?",
+        answer: "C. Escutar ações do usuário (eventos)",
+        options: [
+            "A. Mudar cores no HTML",
+            "B. Criar variáveis",
+            "C. Escutar ações do usuário (eventos)",
+            "D. Ler arquivos",
+        ]
+    },
+    {
+        numb: 10,
+        question: "Qual método é usado para anexar um novo elemento criado (filho) a um elemento pai já existente no DOM?",
+        answer: "B. appendChild()",
+        options: [
+            "A. removeElement()",
+            "B. appendChild()",
+            "C. loadElement()",
+            "D. insertAfter()",
+        ]
+    },
+];
